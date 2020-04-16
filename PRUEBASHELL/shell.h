e185293139c52688626716979c58e44b5ec215df
Fernando Sandoval <sandovbarr@gmail.com>
2020-04-16 11:52:53 -0500

    [ADD]bettycorrections:structdescription

--- PRUEBASHELL/shell.h
+++ PRUEBASHELL/shell.h
@@ -23,6 +23,18 @@
 
 extern char **environ;
 
+/**
+ * struct var - global structure for shell project
+ * @tok: gets tokens of prompt line
+ * @pathtok: gets the tokens of PATH variable
+ * @argv: arguments value of main function
+ * @path: gets the enviroment variable PATH
+ * @concat: gets path enviroment and command on one line
+ * @status: gets the status to be returned
+ * @wcount: count how many times run the prompt
+ * Description: the var struct is a collection of variables used to
+ * build in simple the project, passing arguments between functions easily
+ */
 typedef struct var
 {
 	char **tok;
@@ -30,12 +42,12 @@ typedef struct var
 	char **argv;
 	char *path;
 	char *concat;
-        int status;
+	int status;
 	int wcount;
 } stva;
 
 /* free structure */
-void free_st (stva *var);
+void free_st(stva *var);
 /*---------------*/
 void sig_handler(int signum);
 void _getenv(stva *var, char *word);
