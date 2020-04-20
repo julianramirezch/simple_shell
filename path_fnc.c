b129bc4d0f350fb7f02f4adb5b4ffeada178a489
Fernando Sandoval <sandovbarr@gmail.com>
2020-04-20 14:25:54 -0500

    [FIX]comments

--- path_fnc.c
+++ path_fnc.c
@@ -17,30 +17,30 @@ void _path(stva *var)
 		for (j = 0; var->path[j]; j++)
 		{}
 		string = malloc(sizeof(char) * (j + 1));
-		/*hace una copia del string de entrada*/
+		/*make a copy of the entry string*/
 		j = 0;
 		while (var->path[j])
 			string[i] = var->path[j], i++, j++;
 		string[i] = '\0';
-		/*crea el token para saber cuantas palabras son*/
+		/*made the tokens to know how many words are*/
 		token = strtok(string, ":");
 		while (token != NULL)
 			token = strtok(NULL, ":"), bfc++;
-		/*asigna tamaño a array de palabras / setea variables a 0*/
+		/*malloc size / set variables to 0*/
 		finalargs = malloc(sizeof(char *) * (bfc + 1));
 		i = 0, bfc = 0;
-		/*reasigna los espacios al string de copia*/
+		/*set string again to original*/
 		j = 0;
 		while (var->path[j])
 			string[i] = var->path[j], i++, j++;
 		string[i] = '\0';
-		/*apunta cada palabra a cada posicion del array de palabras*/
+		/*point to each word in each tokens*/
 		token = strtok(var->path, ":");
 		while (token != NULL)
 		{ finalargs[bfc] = token;
 			token = strtok(NULL, ":");
 			bfc++; }
-		/*retorna el puntero a la lista de palabras*/
+		/*returns pointer to works list*/
 		finalargs[bfc] = NULL;
 		var->pathtok = finalargs;
 		free(string);
